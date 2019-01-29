704d5075e2fe5c7ba09171b14836d33503d7cd41
Maria Egorova <moontouched@moontouched.me>
2019-01-29 13:48:09 +0300

    Zynq boot image generation.

--- lib/autobsd/modules/zynq_boot_image.rb
+++ lib/autobsd/modules/zynq_boot_image.rb
@@ -1,27 +1,25 @@
-class Autobsd::Modules::BSDBootImage
+class Autobsd::Modules::ZynqBootImage
   def initialize(builder, config)
     @builder = builder
     @config = config
   end
 
   def build!
-    builder = @builder.exports.fetch "BSDBootImageBuilder"
-
     FileUtils.mkpath File.join(@builder.root, "TargetOutputs")
     image_path = File.join @builder.root, "TargetOutputs", @config.fetch("image_name")
 
-    file = Tempfile.new 'blueprint'
+    file = Tempfile.new 'config'
     begin
-      blueprint = File.read @builder.path_to_file @config.fetch('blueprint')
+      config = File.read @builder.path_to_file @config.fetch('config')
 
-      blueprint.gsub!(/%(.+?)%/) { @builder.exports.fetch $1 }
+      config.gsub!(/%(.+?)%/) { @builder.exports.fetch $1 }
 
-      file.write blueprint
+      file.write config
 
       file.rewind
       file.flush
 
-      @builder.host_execute_checked builder, image_path, file.path
+      @builder.host_execute_checked "#{@builder.config.fetch("xilinx_sdk")}/bin/bootgen", "-arch", "zynq", "-p", @config.fetch("part"), "-image", file.path, "-w", "on", "-o", image_path
     ensure
       file.close
       file.unlink
